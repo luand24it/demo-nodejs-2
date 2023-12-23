e0c24b4dfb31bef883a6b3e733c09db07e866e40
unknown <luand24it@gmail.com>
2023-12-23 09:12:12 +0700

    3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 const PORT = 3001
 
+app.use('/api', (req, res) => res.send('This is API by server demo 2'))
+
 app.use((req, res) => res.send('This is my server demo 2'))
 
 app.listen(PORT, () => console.log('Server running in port ' + PORT))
